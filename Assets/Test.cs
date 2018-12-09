4dcd1bb39198272e723984b9dae1ee6c612c7c8f
Syoya3 <hibitaka1010121@gmail.com>
2018-12-09 21:21:00 +0900

    StepAsideUnityChan初プッシュ

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るモーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
