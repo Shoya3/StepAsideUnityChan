a655fd76f4fc2384b96117a09337a83dc8009d91
Syoya3 <hibitaka1010121@gmail.com>
2018-12-13 23:21:56 +0900

    StepAsideUnityChan
    課題まで完了発展課題は未完

---
+++ Assets/CoinDelete.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinDelete : MonoBehaviour
+{
+    //unitychanのオブジェクトを格納
+    private GameObject unitychan;
+    //unitychanのz座標情報
+    private float unitychanPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        unitychanPos = unitychan.transform.position.z;
+
+        if (this.transform.position.z < unitychanPos - 2.0f)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("コイン消去");
+        }
+    }
+}
---
+++ Assets/ConeDelete.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeDelete : MonoBehaviour
+{
+    //unitychanのオブジェクトを格納
+    private GameObject unitychan;
+    //unitychanのz座標情報
+    private float unitychanPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        unitychanPos = unitychan.transform.position.z;
+
+        if (this.transform.position.z < unitychanPos)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("コーン消去");
+        }
+    }
+}
