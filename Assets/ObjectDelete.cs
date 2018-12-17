74483dcd367816af77c3c17691fc28ee99d6b37b
Syoya3 <hibitaka1010121@gmail.com>
2018-12-17 21:56:57 +0900

    StepAsideUnityChan
    関係ないオブジェクトにアタッチされていたスクリプトを削除
    オブジェクトの破棄が開始される位置をカメラ外になるように変更
    オブジェクトを破棄するスクリプトを１つにし、それぞれのPrefabにアタッチした
    画面外でオブジェクトが破棄されると本当に破棄されているか分からないため、Cosoleに何が破棄されたか表示するスクリプトを追記

---
+++ Assets/ObjectDelete.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDelete : MonoBehaviour {
+    //unitychanのオブジェクトを格納
+    private GameObject unitychan;
+    //unitychanのz座標情報
+    private float unitychanPos;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.unitychan = GameObject.Find("unitychan");
+        unitychanPos = unitychan.transform.position.z;
+
+        if (this.transform.position.z < unitychanPos - 10.0f)
+        {
+            if (this.gameObject.tag == "CarTag")
+            {
+                Destroy(this.gameObject);
+                Debug.Log("車消去");
+            }else if(this.gameObject.tag=="CoinTag"){
+                Destroy(this.gameObject);
+                Debug.Log("コイン消去");
+            }else if (this.gameObject.tag == "TrafficConeTag")
+            {
+                Destroy(this.gameObject);
+                Debug.Log("コーン消去");
+            }
+            
+            
+        }
+    }
+}
